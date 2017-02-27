09f0e8c0df4d8aefd3502be4adfd334f51dd7fc6
drd621 <drdavid621@gmail.com>
2017-02-27 14:34:15 -0600

    Added source code for project

---
+++ Tracker.java
@@ -0,0 +1,14 @@
+/*
+Keto Macro Tracker
+
+Track carbohydrate, protein, and fat intake (macros) commonly associated with ketogenic diets
+Allows user to set daily limits and the ability to reset
+ */
+
+public class Tracker {
+
+    public static void main(String[] args) {
+        Launch launch = new Launch();
+        launch.launchApp();
+    }
+}
