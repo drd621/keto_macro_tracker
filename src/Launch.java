6e979613090c65950036e2648a5a814b54c15406
drd621 <drdavid621@gmail.com>
2017-02-27 18:00:20 -0600

    Update todo list

--- src/Launch.java
+++ src/Launch.java
@@ -1,4 +1,5 @@
 // TODO implement exception handling on menu
+// TODO possible refactor of reset functionality
 
 import java.util.Scanner;
 
@@ -8,27 +9,30 @@ class Launch {
         Protein protein = new Protein();
         Fat fat = new Fat();
         Scanner input = new Scanner(System.in);
-        System.out.println("*MENU*\n");
-        System.out.println("1. Carbs\n2. Protein\n3. Fat\n4. Reset");
-        int choice = input.nextInt();
-        switch(choice) {
-            case 1:
-                carbs.displayCarbs();
-                break;
-            case 2:
-                protein.displayProtein();
-                break;
-            case 3:
-                fat.displayFat();
-                break;
-            case 4:
-                carbs.resetCarbs();
-                protein.resetProtein();
-                fat.resetFat();
-                System.out.println("Macros Reset");
-                break;
-            default:
-                System.out.println("Please enter valid menu option");
+        // TODO implement start/stop
+        while(true) {
+            System.out.println("*MENU*\n");
+            System.out.println("1. Carbs\n2. Protein\n3. Fat\n4. Reset");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    carbs.displayCarbs();
+                    break;
+                case 2:
+                    protein.displayProtein();
+                    break;
+                case 3:
+                    fat.displayFat();
+                    break;
+                case 4:
+                    carbs.resetCarbs();
+                    protein.resetProtein();
+                    fat.resetFat();
+                    System.out.println("Macros Reset");
+                    break;
+                default:
+                    System.out.println("Please enter valid menu option");
+            }
         }
     }
 }
