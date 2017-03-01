8c3fee4337df7f7478eccfcbe8df1b23ad25555a
drd621 <drdavid621@gmail.com>
2017-03-01 15:54:31 -0600

    Commit test

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,8 +1,8 @@
 /*
-Keto Macro Tracker
-
-Track carbohydrate, protein, and fat intake (macros) commonly associated with ketogenic diets
-Allows user to set daily limits and the ability to reset
+ * Keto Macro Tracker
+ *
+ * Track carbohydrate, protein, and fat intake (macros) commonly associated with ketogenic diets
+ * Allows user to set daily limits and the ability to reset
  */
 
 public class Tracker {
